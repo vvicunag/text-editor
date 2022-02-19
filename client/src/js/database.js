d115cbc2d6c0c9e3391b4bd80013dba0ad197e1e
vvicunag <vvicuna1@uc.cl>
2022-02-19 10:21:44 +0100

    imprt header

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,5 +1,8 @@
 import { openDB } from 'idb';
-const dbName = 'jate'
+const dbName = 'jate';
+const storeName = 'jate';
+import { header } from './header';
+
 const initdb = async () =>
   openDB('jate', 1, {
     upgrade(db) {
@@ -13,18 +16,18 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-
+const Key = 1; 
 export const putDb = async (content) => {
   const db = await openDB(dbName);
   const store = db.transaction(storeName, 'readwrite').objectStore(storeName);
-  await store.put({ content, id: contentKey });
+  await store.put({ content, id: Key });
 };
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
   const db = await openDB(dbName);
   const store = db.transaction(storeName, 'readwrite').objectStore(storeName);
-  const data = await store.get(contentKey);
+  const data = await store.get(Key);
   if (!data) {
     return header;
   }
